c8632cd80d045910fc36227bad18dfda827ffa8d
jonghyuk <kasiruske@naver.com>
2023-03-19 13:51:46 +0900

    vue-router setup

---
+++ hacker_news/src/router/index.js
@@ -0,0 +1,28 @@
+
+import {createWebHistory, createRouter} from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
+
+
+const routes = [
+  {
+    path: '/news',
+    component : NewsView
+  },
+  {
+    path: '/ask',
+    component : AskView
+  },
+  {
+    path: '/jobs',
+    component : JobsView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
